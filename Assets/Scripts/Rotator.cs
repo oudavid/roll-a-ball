a27efdeb16fb51e17fe420f607ffb5b81a65a54f
David Ou <david@liffft.com>
2016-12-25 21:21:44 -0800

    Created collectible objects

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,15 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class script : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class Rotator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);	
 	}
 }
